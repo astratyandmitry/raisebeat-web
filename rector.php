d922a5acb4e8bacf21517ac0e0cdf19252c71639
Astratyan Dmitry <astratyandmitry@gmail.com>
2025-08-02 15:20:56 +0500

    Add rector

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php83\Rector\ClassConst\AddTypeToConstRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withRules([
+        DeclareStrictTypesRector::class,
+        AddTypeToConstRector::class,
+        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withPreparedSets(
+
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
