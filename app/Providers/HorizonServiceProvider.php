f239a1a1672d07cd59c5db075f6f65ac85158486
Astratyan Dmitry <astratyandmitry@gmail.com>
2025-08-07 08:20:45 +0500

    Horizon

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+final class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+    }
+
+    protected function authorization(): void
+    {
+        Horizon::auth(function ($request) {
+            return app()->environment('local') || auth('admin')->check();
+        });
+    }
+}
